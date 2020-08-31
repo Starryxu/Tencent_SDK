6e900ca1a9874ebe8e3684c0ef14dbaf1a42ceb1
xuyagung <xuyaguang@insightworks.cn>
2020-08-31 19:09:31 +0800

    no message

---
+++ YGSocialSDK/YGSocialSDK.h
@@ -0,0 +1,25 @@
+//
+//  YGSocialSDK.h
+//  YGSocialSDK
+//
+//  Created by 许公子 on 2020/8/31.
+//  Copyright © 2020 北京聚思汇远科技有限公司. All rights reserved.
+//
+
+#ifndef YGSocialSDK_h
+#define YGSocialSDK_h
+
+/// QQ登录授权+分享
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/QQApiInterface.h>
+#import <TencentOpenAPI/QQApiInterfaceObject.h>
+
+/// 支付宝授权登录
+#import <AFServiceSDK/AFServiceSDK.h>
+
+/// 支付宝分享
+#import "APOpenAPI.h"
+
+
+
+#endif /* YGSocialSDK_h */
